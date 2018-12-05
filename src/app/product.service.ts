5afd824f251c10196fd20b15d6fe9a2c31264be8
vmurdeshwar <vmurdeshwar@seic.com>
2018-12-05 18:43:36 +0530

    Completed the module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -18,8 +18,7 @@ export class ProductService {
   	return this._http.get(this._albumUrl).map((response)=> <Album>response.json());
   }
 
-  getProducts():Observable<Product[]>{
-  	console.log('in service')
+  getProducts():Observable<Product[]>{  
   	return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 
