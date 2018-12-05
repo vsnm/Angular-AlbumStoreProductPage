4d69cdeb510d3718fa22eb060f41117a85a41fd3
vmurdeshwar <vmurdeshwar@seic.com>
2018-12-05 17:21:13 +0530

    Completed the module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,7 +9,7 @@ import {ProductService} from '../product.service';
 })
 export class ProductListComponent implements OnInit {
 
- products:Product;
+ products:Product[];
 
  constructor(private _productService:ProductService) { }
 
